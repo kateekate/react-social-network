51c20fe62dfcbe3b2355ea68fc24a110b6c378a1
Katya <katya.mikhaylova.98@list.ru>
2023-01-07 00:51:21 +0300

    lesson 37

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import { BrowserRouter } from 'react-router-dom';
-import { addPost, updateNewPostText } from './redux/state';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,7 +13,8 @@ export const rerenderEntireTree = () => {
   root.render(
     <BrowserRouter>
       <React.StrictMode>
-        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+        <App state={store.getState()} addPost={store.addPost.bind(store)}
+          updateNewPostText={store.updateNewPostText.bind(store)} />
       </React.StrictMode>
     </BrowserRouter>
   );
@@ -22,9 +23,9 @@ export const rerenderEntireTree = () => {
 
 reportWebVitals();
 
-rerenderEntireTree(state)
+rerenderEntireTree(store.getState())
 
-subscribe(rerenderEntireTree)
+store.subscribe(rerenderEntireTree)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -4,89 +4,90 @@ import ava3 from './../img/avatars/ava3.jpeg'
 import ava4 from './../img/avatars/ava4boy.jpeg'
 import ava5 from './../img/avatars/ava6boy.jpeg'
 
-let rerenderEntireTree = () => {
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        {
+          contain: 'i love my blog',
+          count: 6
+        },
+        {
+          contain: 'let I show u my new pics',
+          count: 20
+        },
+        {
+          contain: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+          count: 1
+        }
+      ],
+      newPostText: 'hehehehehe'
+    },
 
-}
+    dialoguesPage: {
+      dialoguesData: [
+        { id: 1, name: 'Rue' },
+        { id: 2, name: 'Fezco' },
+        { id: 3, name: 'Maddy' },
+        { id: 4, name: 'Nathaniel' },
+        { id: 5, name: 'Jules' },
+      ],
+      messages: [
+        { message: 'ok' },
+        { message: 'wassup boy' },
+        { message: 'u re bitch' },
+        { message: 'love' },
+        { message: 'btw u late' },
+      ]
+    },
 
-let state = {
-  profilePage: {
-    posts: [
-      {
-        contain: 'i love my blog',
-        count: 6
-      },
-      {
-        contain: 'let I show u my new pics',
-        count: 20
-      },
-      {
-        contain: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
-        count: 1
-      }
-    ],
-    newPostText: 'hehehehehe'
+    navBlockFriends: {
+      friends: [
+        {
+          name: 'Rue',
+          img: ava1,
+        },
+        {
+          name: 'Fezco',
+          img: ava4,
+        },
+        {
+          name: 'Maddy',
+          img: ava3,
+        },
+        {
+          name: 'Nathaniel',
+          img: ava5,
+        },
+        {
+          name: 'Jules',
+          img: ava2,
+        },
+      ]
+    }
   },
-
-  dialoguesPage: {
-    dialoguesData: [
-      { id: 1, name: 'Rue' },
-      { id: 2, name: 'Fezco' },
-      { id: 3, name: 'Maddy' },
-      { id: 4, name: 'Nathaniel' },
-      { id: 5, name: 'Jules' },
-    ],
-    messages: [
-      { message: 'ok' },
-      { message: 'wassup boy' },
-      { message: 'u re bitch' },
-      { message: 'love' },
-      { message: 'btw u late' },
-    ]
+  getState() {
+    return this._state
   },
-
-  navBlockFriends: {
-    friends: [
-      {
-        name: 'Rue',
-        img: ava1,
-      },
-      {
-        name: 'Fezco',
-        img: ava4,
-      },
-      {
-        name: 'Maddy',
-        img: ava3,
-      },
-      {
-        name: 'Nathaniel',
-        img: ava5,
-      },
-      {
-        name: 'Jules',
-        img: ava2,
-      },
-    ]
+  _callSubscriber() {
+    console.log('state changed')
+  },
+  addPost() {
+    const newPost = {
+      contain: this._state.profilePage.newPostText,
+      count: 0,
+    }
+    this._state.profilePage.posts.unshift(newPost)
+    this._state.profilePage.newPostText = ''
+    this._callSubscriber(this._state)
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText
+    this._callSubscriber(this._state)
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer
   }
 }
 
-export const addPost = (postMessage) => {
-  const newPost = {
-    contain: postMessage,
-    count: 0,
-  }
-  state.profilePage.posts.unshift(newPost)
-  state.profilePage.newPostText = ''
-  rerenderEntireTree()
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText
-  rerenderEntireTree()
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer
-}
-
-export default state
+export default store
